9d63382ac044b26acfd7b0a52d7760f304634df8
werto87 <huhulasmichinruhe@web.de>
2024-07-25 22:44:07 +0200

    init

--- main.cxx
+++ main.cxx
@@ -1,135 +1,52 @@
-#include <algorithm>
-#include <boost/iterator/iterator_facade.hpp>
-#include <boost/mem_fn.hpp>
-#include <boost/type_traits/is_convertible.hpp>
-#include <boost/utility/enable_if.hpp>
-#include <cassert>
-#include <functional>
+
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
 #include <iostream>
-#include <memory>
-#include <string>
-
-struct node_base
+boost::asio::awaitable<void>
+sendSomething (boost::asio::ip::tcp::endpoint endpoint)
 {
-  node_base () : m_next (0) {}
+  auto ws = boost::beast::websocket::stream<boost::beast::tcp_stream> (co_await boost::beast::net::this_coro::executor);
+  // Set a timeout on the operation
+  boost::beast::get_lowest_layer (ws).expires_after (std::chrono::seconds (30));
 
-  // Each node manages all of its tail nodes
-  virtual ~node_base () { delete m_next; }
+  // Make the connection on the IP address we get from a lookup
+  co_await boost::beast::get_lowest_layer (ws).async_connect (endpoint, boost::asio::use_awaitable);
+  co_await ws.async_handshake (endpoint.address ().to_string () + std::to_string (endpoint.port ()), "/", boost::asio::use_awaitable);
+  auto text = std::string ("abc");
+  auto message = boost::beast::net::buffer (text);
+  co_await ws.async_write (message, boost::asio::use_awaitable);
+  boost::beast::flat_buffer buffer{};
 
-  // Access the rest of the list
-  node_base *
-  next () const
-  {
-    return m_next;
-  }
+  // Read a message into our buffer
+  co_await ws.async_read (buffer, boost::asio::use_awaitable);
 
-  // print to the stream
-  virtual void print (std::ostream &s) const = 0;
+  // Close the WebSocket connection
+  co_await ws.async_close (boost::beast::websocket::close_code::normal, boost::asio::use_awaitable);
 
-  // double the value
-  virtual void double_me () = 0;
+  // If we get here then the connection is closed gracefully
 
-  void
-  append (node_base *p)
-  {
-    if (m_next) m_next->append (p);
-    else
-      m_next = p;
-  }
-
-private:
-  node_base *m_next;
-};
-
-inline std::ostream &
-operator<< (std::ostream &s, node_base const &n)
-{
-  n.print (s);
-  return s;
+  // The make_printable() function helps print a ConstBufferSequence
+  std::cout << boost::beast::make_printable (buffer.data ()) << std::endl;
 }
-
-template <class T> struct node : node_base
-{
-  node (T x) : m_value (x) {}
-
-  void
-  print (std::ostream &s) const
-  {
-    s << this->m_value;
-  }
-  void
-  double_me ()
-  {
-    m_value += m_value;
-  }
-
-private:
-  T m_value;
-};
-
-template <class Value> class node_iter : public boost::iterator_facade<node_iter<Value>, Value, boost::forward_traversal_tag>
-{
-public:
-  node_iter () : m_node (0) {}
-
-  explicit node_iter (Value *p) : m_node (p) {}
-
-private:
-  struct enabler
-  {
-  };
-
-public:
-  template <class OtherValue> node_iter (node_iter<OtherValue> const &other, typename boost::enable_if<boost::is_convertible<OtherValue *, Value *>, enabler>::type = enabler ()) : m_node (other.m_node) {}
-
-private:
-  friend class boost::iterator_core_access;
-  template <class> friend class node_iter;
-
-  template <class OtherValue>
-  bool
-  equal (node_iter<OtherValue> const &other) const
-  {
-    return this->m_node == other.m_node;
-  }
-
-  void
-  increment ()
-  {
-    m_node = m_node->next ();
-  }
-
-  Value &
-  dereference () const
-  {
-    return *m_node;
-  }
-
-  Value *m_node;
-};
-typedef node_iter<node_base> node_iterator;
-typedef node_iter<node_base const> node_const_iterator;
 
 int
 main ()
 {
-  std::unique_ptr<node<int> > nodes (new node<int> (42));
-  nodes->append (new node<std::string> (" is greater than "));
-  nodes->append (new node<int> (13));
-
-  // Check interoperability
-  assert (node_iterator (nodes.get ()) == node_const_iterator (nodes.get ()));
-  assert (node_const_iterator (nodes.get ()) == node_iterator (nodes.get ()));
-
-  assert (node_iterator (nodes.get ()) != node_const_iterator ());
-  assert (node_const_iterator (nodes.get ()) != node_iterator ());
-
-  std::copy (node_iterator (nodes.get ()), node_iterator (), std::ostream_iterator<node_base> (std::cout, " "));
-  std::cout << std::endl;
-
-  std::for_each (node_iterator (nodes.get ()), node_iterator (), boost::mem_fn (&node_base::double_me));
-
-  std::copy (node_const_iterator (nodes.get ()), node_const_iterator (), std::ostream_iterator<node_base> (std::cout, "/"));
-  std::cout << std::endl;
+  auto context = boost::asio::io_context{};
+  auto gameEndpoint = boost::asio::ip::tcp::endpoint{ boost::asio::ip::tcp::v4 (), 4242 };
+  boost::asio::co_spawn (context, sendSomething (gameEndpoint),
+                         // If the awaitable exists with an exception, it gets delivered here as `e`.
+                         // This can happen for regular errors, such as connection drops.
+                         [] (std::exception_ptr e) {
+                           if (e) try
+                               {
+                                 std::rethrow_exception (e);
+                               }
+                             catch (std::exception &ex)
+                               {
+                                 std::cerr << "Error: " << ex.what () << "\n";
+                               }
+                         });
+  context.run ();
   return 0;
 }
